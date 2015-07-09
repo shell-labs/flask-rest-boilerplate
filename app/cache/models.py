73db05a18be1028ccdb9ed4d0789ee77f5a43ad2
Felipe Lalanne <flalanne@niclabs.cl>
2015-07-09 18:46:08 +0200

    Revert "Cleanup etag model"
    This reverts commit 16780d8411b52b5ed8a7b1e9ec73c9d59082bf93.

--- app/cache/models.py
+++ app/cache/models.py
@@ -2,14 +2,30 @@ from __future__ import absolute_import
 from __future__ import unicode_literals
 from app import db
 from app.util import now
-
-import hashlib
+from passlib.hash import sha256_crypt
 
 
 class Etag(db.Model):
     __tablename__ = 'etags'
 
-    uri = db.Column(db.String, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     created = db.Column(db.DateTime, default=now)
     modified = db.Column(db.DateTime, default=now, onupdate=now)
-    value = db.Column(db.String)
+    pk = db.Column(db.String)
+    etag = db.Column(db.String)
+
+    def __init__(self, pk, etag):
+        self.pk = pk
+        self.etag = etag
+
+    @staticmethod
+    def create_etag(data):
+        return sha256_crypt.encrypt(data, salt="samesaltforever")
+
+
+def get_etag(pk):
+    return Etag.query.filter(Etag.pk == pk).first()
+
+def commit_etag(etag):
+    db.session.add(etag)
+    db.session.commit()
