322a47d82b22ef9db4011121c2f3cc198b62ffc7
Felipe Lalanne <flalanne@niclabs.cl>
2015-09-28 17:11:20 -0300

    Merged user forms into auth forms
    Remove authorization form

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -2,9 +2,24 @@ from __future__ import absolute_import
 from __future__ import unicode_literals
 
 from flask.ext.wtf import Form
-from wtforms import SubmitField
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email
+
+from .models import User
 
 
-class AuthorizationForm(Form):
-    yes = SubmitField('yes', )
-    no = SubmitField('no', )
+class LoginForm(Form):
+    email = StringField('email', validators=[DataRequired(), Email()])
+    password = PasswordField('password', validators=[DataRequired()])
+
+    def validate(self):
+        if not Form.validate(self):
+            return False
+
+        user, authenticated = User.authenticate(self.email.data.lower(), self.password.data)
+        if authenticated:
+            self.user = user
+            return True
+        else:
+            self.email.errors.append("Invalid e-mail or password")
+            return False
